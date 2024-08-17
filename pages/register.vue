8618ff98f110f6a121966a14568260ea5f0b3f61
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-08-17 13:53:53 +0700

    refactor: Update registration form with validation rules and data binding

--- pages/register.vue
+++ pages/register.vue
@@ -5,15 +5,32 @@
         <v-toolbar dark color="primary"> Register </v-toolbar>
         <v-card-text>
           <v-form>
-            <v-text-field label="Email" type="email" required></v-text-field>
+            <v-text-field
+              label="Name"
+              :rules="rules.fullname"
+              v-model="form.fullname"
+              type="string"
+              required
+            ></v-text-field>
+            <v-text-field
+              label="Email"
+              :rules="rules.email"
+              v-model="form.email"
+              type="email"
+              required
+            ></v-text-field>
             <v-text-field
               label="Password"
               type="password"
+              :rules="rules.password"
+              v-model="form.password"
               required
             ></v-text-field>
             <v-text-field
               label="Confirm Password"
               type="password"
+              :rules="rules.password_confirmation"
+              v-model="form.password_confirmation"
               required
             ></v-text-field>
           </v-form>
@@ -43,5 +60,33 @@
 <script>
 export default {
   layout: "auth",
+  data() {
+    return {
+      form: {
+        fullname: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+      },
+      rules: {
+        fullname: [(v) => !!v || "Fullname is required"],
+        email: [
+          (v) => !!v || "E-mail is required",
+          (v) =>
+            /[a-z0-9]+@[a-z}+.[a-z]{2,3}/.test(v) || "E-mail must be valid",
+        ],
+        password: [
+          (v) => !!v || "Password is required",
+          (v) => v.length >= 6 || "Password must be more than 6 characters",
+        ],
+        password_confirmation: [
+          (v) => !!v || "Password Confirmation is required",
+          (v) =>
+            v === this.form.password ||
+            "Password Confirmation must be the same as Password",
+        ],
+      },
+    };
+  },
 };
 </script>
