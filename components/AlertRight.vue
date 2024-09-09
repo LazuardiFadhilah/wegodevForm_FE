41ddb78d72d51ba2fe49b38f991635c5195057e8
Lazuardi Fadhilah <Lazuardifadhillah@gmail.com>
2024-09-09 20:33:36 +0700

    refactor: Update routes.json and AlertRight component

--- components/AlertRight.vue
+++ components/AlertRight.vue
@@ -1,10 +1,52 @@
 <template>
   <div class="v-alert-right">
-    <v-alert outlined border="left" type="error" class="v-bg-white">
-      test
+    <v-alert
+      outlined
+      border="left"
+      :type="alert.type"
+      v-model="alert.show"
+      class="v-bg-white"
+      v-for="(alert, index) in alerts"
+      :key="index"
+    >
+      <v-row align="center">
+        <v-col class="grow"> {{ alert.message }} </v-col>
+        <v-col class="shrink">
+          <v-btn icon :color="alert.type" @click="onClose(index)">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-alert>
   </div>
 </template>
+
+<script>
+export default {
+  name: "AlertRight",
+  data() {
+    return {
+      alerts: [
+        {
+          message: "Something went wrong",
+          type: "error",
+          show: true,
+        },
+        {
+          message: "something wrong happened",
+          type: "success",
+          show: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    onClose(index) {
+      this.alerts[index].show = false;
+    },
+  },
+};
+</script>
 
 <style scoped>
 .v-bg-white {
